d901788dc0cb116ccca7c2acc94e8908b5022a5e
tiopacange-cmyk <tiopacange@gmail.com>
2026-09-23 18:06:17 +0000

    V2 : sauvegardes automatiques planifiees

--- delotrans-ged/routes/console.php
+++ delotrans-ged/routes/console.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// ===== Sauvegardes automatiques (V2) =====
+Illuminate\Support\Facades\Schedule::call(function () {
+    app(App\Services\BackupService::class)->run('database', null, null);
+})->dailyAt('02:00')->name('sauvegarde-base-quotidienne');
+
+Illuminate\Support\Facades\Schedule::call(function () {
+    app(App\Services\BackupService::class)->run('config', null, null);
+})->weeklyOn(0, '03:00')->name('sauvegarde-config-hebdo');
